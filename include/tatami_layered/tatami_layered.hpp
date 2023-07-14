144435839dc314dfb09727231e11daffa5bc4fb4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-07-14 09:28:18 -0700

    Added an umbrella header for easier includes.

---
+++ include/tatami_layered/tatami_layered.hpp
@@ -0,0 +1,18 @@
+#ifndef TATAMI_LAYERED_TATAMI_LAYERED_HPP
+#define TATAMI_LAYERED_TATAMI_LAYERED_HPP
+
+#include "convert_to_layered_sparse.hpp"
+#include "read_layered_sparse_from_matrix_market.hpp"
+
+/**
+ * @file tatami_layered.hpp
+ * @brief Umbrella header for the **tatami_layered** library.
+ */
+
+/**
+ * @namespace tatami_layered
+ * @brief Create layered sparse matrices for **tatami**.
+ */
+namespace tatami_layered {}
+
+#endif
