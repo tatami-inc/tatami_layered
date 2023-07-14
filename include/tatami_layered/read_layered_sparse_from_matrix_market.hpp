2ca32f00b2f48dc61405d10c2c3d04925b97f4d2
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2023-07-14 09:18:00 -0700

    Added Matrix Market readers (restored from old tatami code). (#1)
    This directly reads from a (possibly Gzipped) Matrix Market file into
    the layered sparse format without any in-memory intermediate, at the
    cost of having to perform two passes through the file.

---
+++ include/tatami_layered/read_layered_sparse_from_matrix_market.hpp
@@ -0,0 +1,298 @@
+#ifndef TATAMI_LAYERED_READ_LAYERED_SPARSE_FROM_MATRIX_MARKET_HPP
+#define TATAMI_LAYERED_READ_LAYERED_SPARSE_FROM_MATRIX_MARKET_HPP
+
+#include "byteme/byteme.hpp"
+#include "eminem/eminem.hpp"
+
+#include <vector>
+#include <algorithm>
+
+#include "utils.hpp"
+
+/**
+ * @file read_layered_sparse_from_matrix_market.hpp
+ * @brief Read layered sparse matrices from Matrix Market files.
+ */
+
+namespace tatami_layered {
+
+/**
+ * @cond
+ */
+template<typename Value_, typename Index_, typename ColumnIndex_, class Creator_>
+std::shared_ptr<tatami::Matrix<Value_, Index_> > read_layered_sparse_from_matrix_market(Creator_ create, Index_ chunk_size) {
+    size_t NR, NC, nchunks, leftovers;
+
+    std::vector<Holder<uint8_t, Index_, ColumnIndex_> > store8;
+    std::vector<Holder<uint16_t, Index_, ColumnIndex_> > store16;
+    std::vector<Holder<uint32_t, Index_, ColumnIndex_> > store32;
+
+    std::vector<std::vector<Index_> > identities8, identities16, identities32;
+    std::vector<std::vector<Index_> > assigned_position;
+    std::vector<std::vector<Category> > assigned_category;
+
+    // First pass, scanning for the max and number.
+    {
+        auto reader = create();
+        eminem::Parser parser(&reader);
+
+        parser.scan_preamble();
+        NR = parser.get_nrows();
+        NC = parser.get_ncols();
+        nchunks = std::max(static_cast<size_t>(1), NC / chunk_size + (leftovers != 0));
+        leftovers = NC % chunk_size;
+
+        store8.resize(nchunks);
+        store16.resize(nchunks);
+        store32.resize(nchunks);
+        identities8.resize(nchunks);
+        identities16.resize(nchunks);
+        identities32.resize(nchunks);
+        assigned_position.resize(nchunks);
+        assigned_category.resize(nchunks);
+
+        std::vector<std::vector<Category> > max_per_chunk(nchunks);
+        std::vector<std::vector<Index_> > num_per_chunk(nchunks);
+        for (auto& x : max_per_chunk) { x.resize(NR); }
+        for (auto& x : num_per_chunk) { x.resize(NR); }
+
+        auto handler = [&](size_t r, size_t c, int val) -> void {
+            auto chunk = (c - 1) / chunk_size;
+            --r;
+            max_per_chunk[chunk][r] = std::max(max_per_chunk[chunk][r], categorize(val));
+            ++num_per_chunk[chunk][r];
+        };
+
+        const auto& banner = parser.get_banner();
+        if (banner.field == eminem::Field::INTEGER) {
+            parser.scan_integer(handler);
+        } else if (banner.field == eminem::Field::DOUBLE || banner.field == eminem::Field::REAL) {
+            parser.scan_real(handler);
+        } else {
+            throw std::runtime_error("expected a numeric field in the Matrix Market file");
+        }
+
+        allocate_rows(
+            max_per_chunk, 
+            num_per_chunk, 
+            identities8, 
+            identities16, 
+            identities32, 
+            store8, 
+            store16, 
+            store32, 
+            assigned_category, 
+            assigned_position
+        );
+    }
+   
+    // Now allocating.
+    {
+        std::vector<std::vector<size_t> > output_positions(nchunks);
+        for (size_t chunk = 0; chunk < nchunks; ++chunk) {
+            output_positions[chunk].resize(NR);
+            for (size_t r = 0; r < NR; ++r) {
+                output_positions[chunk][r] = get_sparse_ptr(store8, store16, store32, assigned_category, assigned_position, chunk, static_cast<Index_>(r));
+            }
+        }
+
+        auto handler = [&](size_t r, size_t c, int val) -> void {
+            --c;
+            auto chunk = c / chunk_size;
+            Index_ offset = c % chunk_size;
+            --r;
+            fill_sparse_value(store8, store16, store32, assigned_category[chunk][r], chunk, offset, val, output_positions[chunk][r]++);
+        };
+
+        auto reader = create();
+        eminem::Parser parser(&reader);
+        parser.scan_preamble();
+
+        const auto& banner = parser.get_banner();
+        if (banner.field == eminem::Field::INTEGER) {
+            parser.scan_integer(handler);
+        } else if (banner.field == eminem::Field::DOUBLE || banner.field == eminem::Field::REAL) {
+            parser.scan_real(handler);
+        }
+
+        // Checking that the column indices are sorted properly.
+        auto sorter = [&](auto& store) -> void {
+            std::vector<std::pair<typename decltype(store[0].index)::value_type, typename decltype(store[0].value)::value_type> > buffer;
+            buffer.reserve(chunk_size);
+
+            for (auto& st : store) {
+                size_t current_NR = st.ptr.size() - 1;
+                for (size_t r = 0; r < current_NR; ++r) {
+                    size_t start = st.ptr[r], end = st.ptr[r + 1];
+
+                    if (!std::is_sorted(st.index.begin() + start, st.index.begin() + end)) {
+                        buffer.clear();
+                        for (size_t i = start; i < end; ++i) {
+                            buffer.emplace_back(st.index[i], st.value[i]);
+                        }
+
+                        std::sort(buffer.begin(), buffer.end());
+                        auto bIt = buffer.begin();
+                        for (size_t i = start; i < end; ++i, ++bIt) {
+                            st.index[i] = bIt->first;
+                            st.value[i] = bIt->second;
+                        }
+                    }
+                }
+            }
+        };
+
+        sorter(store8);
+        sorter(store16);
+        sorter(store32);
+    }
+
+    return consolidate_matrices<Value_, Index_>(
+        identities8, 
+        identities16, 
+        identities32, 
+        std::move(store8), 
+        std::move(store16), 
+        std::move(store32),
+        static_cast<Index_>(NR),
+        chunk_size,
+        static_cast<Index_>(leftovers)
+    );
+}
+/**
+ * @endcond
+ */
+
+/**
+ * @param filepath Path to an uncompressed Matrix Market text file.
+ * @param chunk_size Chunk size to use for partitioning columns.
+ * @param buffer_size Size of the buffer (in bytes) to use when reading from file.
+ * 
+ * @return A `tatami::Matrix` object containing a layered sparse matrix.
+ *
+ * @tparam Value_ Type of data value for the output `tatami::Matrix` interface.
+ * @tparam Index_ Integer type for the row/column indices of the output.
+ * @tparam ColumnIndex_ Integer type for the stored column indices.
+ *
+ * This function loads a layered sparse integer matrix from a Matrix Market file.
+ * The aim is to reduce memory usage by storing each gene's counts in the smallest unsigned integer type that can hold them.
+ * See `convert_to_layered_sparse()` for more details.
+ */
+template<typename Value_ = double, typename Index_ = int, typename ColumnIndex_ = uint16_t>
+std::shared_ptr<tatami::Matrix<Value_, Index_> > read_layered_sparse_from_matrix_market_text_file(const char * filepath, Index_ chunk_size = 65536, size_t buffer_size = 65536) {
+    return read_layered_sparse_from_matrix_market<Value_, Index_, ColumnIndex_>([&]() -> auto { return byteme::RawFileReader(filepath, buffer_size); }, chunk_size);
+}
+
+#if __has_include("zlib.h")
+
+/**
+ * @param filepath Path to a (possibly Gzip-compressed) Matrix Market file.
+ * @param chunk_size Chunk size to use for partitioning columns.
+ * @param buffer_size Size of the buffer (in bytes) to use when reading from file.
+ * 
+ * @return A `tatami::Matrix` object containing a layered sparse matrix.
+ *
+ * @tparam Value_ Type of data value for the output `tatami::Matrix` interface.
+ * @tparam Index_ Integer type for the row/column indices of the output.
+ * @tparam ColumnIndex_ Integer type for the stored column indices.
+ *
+ * This function loads a layered sparse integer matrix from a Matrix Market file.
+ * The aim is to reduce memory usage by storing each gene's counts in the smallest unsigned integer type that can hold them.
+ * See `convert_to_layered_sparse()` for more details.
+ */
+template<typename Value_ = double, typename Index_ = int, typename ColumnIndex_ = uint16_t>
+std::shared_ptr<tatami::Matrix<Value_, Index_> > read_layered_sparse_from_matrix_market_some_file(const char * filepath, Index_ chunk_size = 65536, size_t buffer_size = 65536) {
+    return read_layered_sparse_from_matrix_market<Value_, Index_, ColumnIndex_>([&]() -> auto { return byteme::SomeFileReader(filepath, buffer_size); }, chunk_size);
+}
+
+/**
+ * @param filepath Path to a Gzip-compressed Matrix Market file.
+ * @param chunk_size Chunk size to use for partitioning columns.
+ * @param buffer_size Size of the buffer (in bytes) to use when reading from file.
+ * 
+ * @return A `tatami::Matrix` object containing a layered sparse matrix.
+ *
+ * @tparam Value_ Type of data value for the output `tatami::Matrix` interface.
+ * @tparam Index_ Integer type for the row/column indices of the output.
+ * @tparam ColumnIndex_ Integer type for the stored column indices.
+ *
+ * This function loads a layered sparse integer matrix from a Matrix Market file.
+ * The aim is to reduce memory usage by storing each gene's counts in the smallest unsigned integer type that can hold them.
+ * See `convert_to_layered_sparse()` for more details.
+ */
+template<typename Value_ = double, typename Index_ = int, typename ColumnIndex_ = uint16_t>
+std::shared_ptr<tatami::Matrix<Value_, Index_> > read_layered_sparse_from_matrix_market_gzip_file(const char * filepath, Index_ chunk_size = 65536, size_t buffer_size = 65536) {
+    return read_layered_sparse_from_matrix_market<Value_, Index_, ColumnIndex_>([&]() -> auto { return byteme::GzipFileReader(filepath, buffer_size); }, chunk_size);
+}
+
+#endif
+
+/**
+ * @param contents Array containing the contents of an uncompressed Matrix Market text file.
+ * @param length Length of the array.
+ * @param chunk_size Chunk size to use for partitioning columns.
+ * 
+ * @return A `tatami::Matrix` object containing a layered sparse matrix.
+ *
+ * @tparam Value_ Type of data value for the output `tatami::Matrix` interface.
+ * @tparam Index_ Integer type for the row/column indices of the output.
+ * @tparam ColumnIndex_ Integer type for the stored column indices.
+ *
+ * This function loads a layered sparse integer matrix from a buffer with the contents of a Matrix Market file.
+ * The aim is to reduce memory usage by storing each gene's counts in the smallest unsigned integer type that can hold them.
+ * See `convert_to_layered_sparse()` for more details.
+ */
+template<typename Value_ = double, typename Index_ = int, typename ColumnIndex_ = uint16_t>
+std::shared_ptr<tatami::Matrix<Value_, Index_> > read_layered_sparse_from_matrix_market_text_buffer(const unsigned char* contents, size_t length, Index_ chunk_size = 65536) {
+    return read_layered_sparse_from_matrix_market<Value_, Index_, ColumnIndex_>([&]() -> auto { return byteme::RawBufferReader(contents, length); }, chunk_size);
+}
+
+#if __has_include("zlib.h")
+
+/**
+ * @param contents Array containing the contents of a (possibly Gzip/Zlib-compressed) Matrix Market file.
+ * @param length Length of the array.
+ * @param chunk_size Chunk size to use for partitioning columns.
+ * @param buffer_size Size of the buffer to use for decompression, in bytes.
+ * 
+ * @return A `tatami::Matrix` object containing a layered sparse matrix.
+ *
+ * @tparam Value_ Type of data value for the output `tatami::Matrix` interface.
+ * @tparam Index_ Integer type for the row/column indices of the output.
+ * @tparam ColumnIndex_ Integer type for the stored column indices.
+ *
+ * This function loads a layered sparse integer matrix from a buffer with the contents of a Matrix Market file.
+ * The aim is to reduce memory usage by storing each gene's counts in the smallest unsigned integer type that can hold them.
+ * See `convert_to_layered_sparse()` for more details.
+ */
+template<typename Value_ = double, typename Index_ = int, typename ColumnIndex_ = uint16_t>
+std::shared_ptr<tatami::Matrix<Value_, Index_> > read_layered_sparse_from_matrix_market_some_buffer(const unsigned char* contents, size_t length, Index_ chunk_size = 65536, size_t buffer_size = 65536) {
+    return read_layered_sparse_from_matrix_market<Value_, Index_, ColumnIndex_>([&]() -> auto { return byteme::SomeBufferReader(contents, length, buffer_size); }, chunk_size);
+}
+
+/**
+ * @param contents Array containing the contents of a Gzip/Zlib-compressed Matrix Market file.
+ * @param length Length of the array.
+ * @param chunk_size Chunk size to use for partitioning columns.
+ * @param buffer_size Size of the buffer to use for decompression, in bytes.
+ * 
+ * @return A `tatami::Matrix` object containing a layered sparse matrix.
+ *
+ * @tparam Value_ Type of data value for the output `tatami::Matrix` interface.
+ * @tparam Index_ Integer type for the row/column indices of the output.
+ * @tparam ColumnIndex_ Integer type for the stored column indices.
+ *
+ * This function loads a layered sparse integer matrix from a buffer with the contents of a Matrix Market file.
+ * The aim is to reduce memory usage by storing each gene's counts in the smallest unsigned integer type that can hold them.
+ * See `convert_to_layered_sparse()` for more details.
+ */
+template<typename Value_ = double, typename Index_ = int, typename ColumnIndex_ = uint16_t>
+std::shared_ptr<tatami::Matrix<Value_, Index_> > read_layered_sparse_from_matrix_market_zlib_buffer(const unsigned char* contents, size_t length, Index_ chunk_size = 65536, size_t buffer_size = 65536) {
+    return read_layered_sparse_from_matrix_market<Value_, Index_, ColumnIndex_>([&]() -> auto { return byteme::SomeBufferReader(contents, length, buffer_size); }, chunk_size);
+}
+
+#endif
+
+}
+
+#endif
---
+++ tests/src/read_layered_sparse_from_matrix_market.cpp
@@ -0,0 +1,142 @@
+#include <gtest/gtest.h>
+
+#include "tatami/tatami.hpp"
+#include "tatami_layered/read_layered_sparse_from_matrix_market.hpp"
+#include "byteme/temp_file_path.hpp"
+
+#include "mock_layered_sparse_data.h"
+
+#include <fstream>
+#include <sstream>
+
+class ReadLayeredSparseFromMatrixMarketTest : public ::testing::TestWithParam<std::tuple<bool, int> > {
+protected:
+    size_t NR = 2000, NC = 1000;
+
+    template<class Stream, class U, class V, class W>
+    static void write_matrix_market(Stream& stream, size_t nr, size_t nc, const U& vals, const V& rows, const W& cols, bool scramble) {
+        stream << "%%MatrixMarket matrix coordinate integer general\n";
+        stream << nr << " " << nc << " " << vals.size();
+
+        if (scramble) {
+            std::vector<size_t> reorder(vals.size());
+            std::iota(reorder.begin(), reorder.end(), 0);
+            std::mt19937_64 rng(vals.size() - nr + nc);
+            std::shuffle(reorder.begin(), reorder.end(), rng);
+            for (auto i : reorder) {
+                stream << "\n" << rows[i] + 1 << " " << cols[i] + 1 << " " << vals[i];
+            }
+
+        } else {
+            for (size_t i = 0; i < vals.size(); ++i) {
+                stream << "\n" << rows[i] + 1 << " " << cols[i] + 1 << " " << vals[i];
+            }
+        }
+        stream << "\n";
+
+        return;
+    }
+};
+
+TEST_P(ReadLayeredSparseFromMatrixMarketTest, File) {
+    auto param = GetParam();
+    auto scrambled = std::get<0>(param);
+    auto compressed = std::get<1>(param);
+
+    std::vector<size_t> rows, cols;
+    std::vector<int> vals;
+    mock_layered_sparse_data<false>(NR, NC, rows, cols, vals);
+
+    auto indptrs = tatami::compress_sparse_triplets<false>(NR, NC, vals, rows, cols);
+    typedef tatami::CompressedSparseColumnMatrix<double, int, decltype(vals), decltype(rows), decltype(indptrs)> SparseMat; 
+    auto ref = std::shared_ptr<tatami::NumericMatrix>(new SparseMat(NR, NC, vals, rows, indptrs)); 
+
+    auto path = byteme::temp_file_path("tatami-tests-ext-MatrixMarket", ".mtx");
+    if (!compressed) {
+        std::ofstream file_out(path);
+        write_matrix_market(file_out, NR, NC, vals, rows, cols, scrambled);
+    } else {
+        std::stringstream sstream;
+        write_matrix_market(sstream, NR, NC, vals, rows, cols, scrambled);
+        auto contents = sstream.str();
+
+        path += ".gz";
+        gzFile ohandle = gzopen(path.c_str(), "w");
+        gzwrite(ohandle, contents.data(), contents.size());
+        gzclose(ohandle);
+    }
+
+    std::shared_ptr<tatami::NumericMatrix> out;
+    if (compressed == 0) {
+        out = tatami_layered::read_layered_sparse_from_matrix_market_text_file(path.c_str());
+    } else if (compressed == 1) {
+        out = tatami_layered::read_layered_sparse_from_matrix_market_gzip_file(path.c_str());
+    } else if (compressed == 2) {
+        out = tatami_layered::read_layered_sparse_from_matrix_market_some_file(path.c_str());
+    }
+
+    auto owrk = out->dense_row();
+    auto rwrk = ref->dense_row();
+    for (size_t i = 0; i < NR; ++i) {
+        auto stuff = owrk->fetch(i);
+        EXPECT_EQ(stuff, rwrk->fetch(i));
+    }
+}
+
+TEST_P(ReadLayeredSparseFromMatrixMarketTest, Buffer) {
+    auto param = GetParam();
+    auto scrambled = std::get<0>(param);
+    auto compressed = std::get<1>(param);
+
+    std::vector<size_t> rows, cols;
+    std::vector<int> vals;
+    mock_layered_sparse_data<false>(NR, NC, rows, cols, vals);
+
+    auto indptrs = tatami::compress_sparse_triplets<false>(NR, NC, vals, rows, cols);
+    typedef tatami::CompressedSparseColumnMatrix<double, int, decltype(vals), decltype(rows), decltype(indptrs)> SparseMat; 
+    auto ref = std::shared_ptr<tatami::NumericMatrix>(new SparseMat(NR, NC, vals, rows, indptrs)); 
+
+    std::stringstream buf_out;
+    write_matrix_market(buf_out, NR, NC, vals, rows, cols, scrambled);
+    auto contents = buf_out.str();
+
+    if (compressed) {
+        auto path = byteme::temp_file_path("tatami-tests-ext-MatrixMarket", ".mtx.gz");
+        gzFile ohandle = gzopen(path.c_str(), "w");
+        gzwrite(ohandle, contents.data(), contents.size());
+        gzclose(ohandle);
+
+        std::ifstream handle(path, std::ios_base::binary);
+        handle >> std::noskipws;
+        contents.clear();
+        contents.insert(contents.end(), std::istream_iterator<unsigned char>{handle}, std::istream_iterator<unsigned char>());
+    }
+
+    std::shared_ptr<tatami::NumericMatrix> out;
+    auto ptr = reinterpret_cast<const unsigned char*>(contents.data());
+    size_t n = contents.size();
+
+    if (compressed == 0) {
+        out = tatami_layered::read_layered_sparse_from_matrix_market_text_buffer(ptr, n);
+    } else if (compressed == 1) {
+        out = tatami_layered::read_layered_sparse_from_matrix_market_zlib_buffer(ptr, n);
+    } else if (compressed == 2) {
+        out = tatami_layered::read_layered_sparse_from_matrix_market_some_buffer(ptr, n);
+    }
+
+    auto owrk = out->dense_row();
+    auto rwrk = ref->dense_row();
+    for (size_t i = 0; i < NR; ++i) {
+        auto stuff = owrk->fetch(i);
+        EXPECT_EQ(stuff, rwrk->fetch(i));
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ReadLayeredSparseFromMatrixMarket,
+    ReadLayeredSparseFromMatrixMarketTest,
+    ::testing::Combine(
+        ::testing::Values(false, true), // scrambled?
+        ::testing::Values(0, 1, 2)  // Gzipped?
+    )
+);
